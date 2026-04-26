f3efa9a974bf91f2f95fe715a12ae79634f02121
schplorg <git@schplorg.de>
2026-04-26 22:14:02 +0200

    asd

--- stop-wolf.sh
+++ stop-wolf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-podman ps -a --format "{{.Names}}" | grep -i '^wolf' | xargs -r podman rm -f
-podman container prune -f
-podman pod prune -f
+sudo podman ps -a --format "{{.Names}}" | grep -i '^wolf' | xargs -r sudo podman rm -f
+sudo podman container prune -f
+sudo podman pod prune -f
