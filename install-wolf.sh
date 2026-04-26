efd06dbe0b42bc82d7f6c5fe4c3192fa55af8924
schplorg <git@schplorg.de>
2026-04-26 23:32:16 +0200

    sdf

--- install-wolf.sh
+++ install-wolf.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-instances=(
-  "peer_one:10.13.13.2"
-)
-sudo podman ps -a --format "{{.Names}}" | grep -i '^wolf' | xargs -r sudo podman rm -f
-for entry in "${instances[@]}"; do
-  INSTANCE="${entry%%:*}"
-  BIND_IP="${entry##*:}"
-  sudo mkdir -p "/etc/wolf-${INSTANCE}"
-  echo "Starting wolf-${INSTANCE} on ${BIND_IP}..."
-  sudo INSTANCE="${INSTANCE}" BIND_IP="${BIND_IP}" podman compose \
-    --project-name "wolf-${INSTANCE}" \
-    up -d
-done
+sudo podman ps -a --format "{{.Names}}" | grep -i '^wolf' | xargs -r sudo podman rm -f || true
+sudo podman compose up -d
