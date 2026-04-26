f30fb039eb35035330f8de9e40430e5d7fa0bb36
schplorg <git@schplorg.de>
2026-04-26 19:35:04 +0200

    as

--- install-wolf.sh
+++ install-wolf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo podman ps -a --format "{{.Names}}" | grep -i '^wolf' | xargs -r podman rm -f || true
+sudo podman ps -a --format "{{.Names}}" | grep -i '^wolf' | xargs -r sudo podman rm -f || true
 sudo podman compose up -d
