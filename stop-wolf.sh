1e1fdaa73502a12bbcf32e11be4ff2aaca85b1ee
schplorg <git@schplorg.de>
2026-04-27 13:49:48 +0200

    asd

--- stop-wolf.sh
+++ stop-wolf.sh
@@ -4,4 +4,9 @@ for i in 1 2 3 4; do
   ps -a --format "{{.Names}}" \
   | xargs -r podman --root /var/lib/wolf${i} --runroot /run/wolf${i} \
   rm -f || true
-end
+  systemctl stop --now podman-wolf${i} || true
+  systemctl disable --now podman-wolf${i} || true
+  rm -rf /etc/systemd/system/podman-wolf${i}.service
+  systemctl daemon-reload
+  rm -rf /run/wolf${i} /var/lib/wolf${i}
+done
