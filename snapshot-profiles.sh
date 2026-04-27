54c3274e92d52988b9287b2f05f04cab2beb0c74
schplorg <git@schplorg.de>
2026-04-27 20:45:12 +0200

    asd

--- snapshot-profiles.sh
+++ snapshot-profiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-template1=/etc/wolf/lutris1
+template1=/etc/wolf/lutris-template
 targets1=(
+  /etc/wolf/lutris1
   /etc/wolf/lutris2
   /etc/wolf/lutris3
   /etc/wolf/lutris4
@@ -9,16 +10,22 @@ targets1=(
 
 for d in "${targets1[@]}"; do
   [ -e "$d" ] && btrfs subvolume delete "$d"
+  if [ -e "$d" ]; then
+    rm -rf "$d"
+  fi
   btrfs subvolume snapshot $template1 "$d"
 done
 
-template2=/etc/wolf/profile-data/user1
+template2=/etc/wolf/profile-data/user-template
 targets2=(
-  /etc/wolf/profile-data/user2
+  /etc/wolf/profile-data/user1
   /etc/wolf/profile-data/user3
   /etc/wolf/profile-data/user4
 )
 for d in "${targets2[@]}"; do
   [ -e "$d" ] && btrfs subvolume delete "$d"
+  if [ -e "$d" ]; then
+    rm -rf "$d"
+  fi
   btrfs subvolume snapshot $template2 "$d"
 done
