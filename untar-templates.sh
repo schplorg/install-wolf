382b46c15e13a8b4aba9b4674ff73c68694c76af
schplorg <git@schplorg.de>
2026-04-28 15:13:34 +0200

    asd

--- untar-templates.sh
+++ untar-templates.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-read -p "Extract templates? [y/N] " confirm
-[[ "$confirm" != [yY] ]] && exit 0
-
-tar -xf ./templates/lutris-template.tar -C /etc/wolf/lutris-template
-tar -xf ./templates/user-template.tar -C /etc/wolf/profile-data/user-template
-
-echo "Done."
+tar -xf ./templates/lutris-template.tar -C /etc/wolf/
+tar -xf ./templates/user-template.tar -C /etc/wolf/profile-data/
