a6922714164f7f6b6bc6de642ad63982e67a8e4d
schplorg <git@schplorg.de>
2026-04-28 14:05:43 +0200

    asd

--- create-macvlan.sh
+++ create-macvlan.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-read -p "Use podman? (y/n): " USE_PODMAN
+set -euo pipefail
+source .env
 
-if [[ "$USE_TOOLKIT" == "y" ]]; then
-  podman network create \
-    --driver macvlan \
-    --opt parent=enp5s0 \
-    --subnet 192.168.42.0/24 \
-    --gateway 192.168.42.1 \
-    wolf_macvlan
-else
-  docker network create \
-    --driver macvlan \
-    --opt parent=enp5s0 \
-    --subnet 192.168.42.0/24 \
-    --gateway 192.168.42.1 \
-    wolf_macvlan
-fi
+$CONTAINER_TOOL network create \
+  --driver macvlan \
+  --opt parent=enp5s0 \
+  --subnet 192.168.42.0/24 \
+  --gateway 192.168.42.1 \
+  wolf_macvlan
