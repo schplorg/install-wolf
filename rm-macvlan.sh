dc1b0a36af81fe90e722322dfb1e2a267196b016
schplorg <git@schplorg.de>
2026-05-02 14:06:14 +0200

    asd

--- rm-macvlan.sh
+++ rm-macvlan.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 
 source .env
 
-$CONTAINER_TOOL rm -f $($CONTAINER_TOOL ps -a --format "{{.Names}}" | grep -iE '^wolf') || true
+$CONTAINER_TOOL network rm wolf_macvlan 2>/dev/null || true
