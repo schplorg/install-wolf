d18a4dc333fe4562eb78b6380e53b6f82ef9cc5e
schplorg <git@schplorg.de>
2026-05-06 16:14:31 +0200

    asd

--- 99-startup.sh
+++ 99-startup.sh
@@ -3,15 +3,15 @@
 # echo 'for_window [app_id=".*"] fullscreen enable' >> ~/.config/sway/custom-cfg
 # echo 'for_window [class=".*"] fullscreen enable' >> ~/.config/sway/custom-cfg
 
-echo 'bindsym Mod4+f fullscreen toggle' >> ~/.config/sway/custom-cfg
-echo 'bindsym Mod4+shift+f fullscreen toggle global' >> ~/.config/sway/custom-cfg
+# echo 'bindsym Mod4+f fullscreen toggle' >> ~/.config/sway/custom-cfg
+# echo 'bindsym Mod4+shift+f fullscreen toggle global' >> ~/.config/sway/custom-cfg
 
-echo 'bindsym Mod4+Tab focus next' >> ~/.config/sway/custom-cfg
-echo 'bindsym Mod4+shift+Tab focus prev' >> ~/.config/sway/custom-cfg
-echo 'bindsym alt+Tab focus next' >> ~/.config/sway/custom-cfg
-echo 'bindsym alt+shift+Tab focus prev' >> ~/.config/sway/custom-cfg
+# echo 'bindsym Mod4+Tab focus next' >> ~/.config/sway/custom-cfg
+# echo 'bindsym Mod4+shift+Tab focus prev' >> ~/.config/sway/custom-cfg
+# echo 'bindsym alt+Tab focus next' >> ~/.config/sway/custom-cfg
+# echo 'bindsym alt+shift+Tab focus prev' >> ~/.config/sway/custom-cfg
 
-echo 'bindsym Mod4+m fullscreen disable' >> ~/.config/sway/custom-cfg
+# echo 'bindsym Mod4+m fullscreen disable' >> ~/.config/sway/custom-cfg
 
 # echo 'bindsym Mod4+1 workspace number 1' >> ~/.config/sway/custom-cfg
 # echo 'bindsym Mod4+2 workspace number 2' >> ~/.config/sway/custom-cfg
