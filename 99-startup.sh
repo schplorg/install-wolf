a5309240ef0cf6a86582711c27e60d521175f8ca
schplorg <git@schplorg.de>
2026-05-06 14:23:29 +0200

    add

--- 99-startup.sh
+++ 99-startup.sh
@@ -12,3 +12,22 @@ echo 'bindsym alt+Tab focus next' >> ~/.config/sway/custom-cfg
 echo 'bindsym alt+shift+Tab focus prev' >> ~/.config/sway/custom-cfg
 
 echo 'bindsym Mod4+m fullscreen disable' >> ~/.config/sway/custom-cfg
+
+echo 'bindsym Mod4+1 workspace number 1' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+2 workspace number 2' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+3 workspace number 3' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+4 workspace number 4' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+5 workspace number 5' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+left workspace prev' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+right workspace next' >> ~/.config/sway/custom-cfg
+
+echo 'bindsym Mod4+shift+1 move container to workspace number 1' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+shift+2 move container to workspace number 2' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+shift+3 move container to workspace number 3' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+shift+4 move container to workspace number 4' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+shift+5 move container to workspace number 5' >> ~/.config/sway/custom-cfg
+
+echo 'bindsym Mod4+d exec wofi --show drun' >> ~/.config/sway/custom-cfg
+echo 'bindsym Mod4+return exec foot' >> ~/.config/sway/custom-cfg
+
+echo 'bindsym Mod4+q kill' >> ~/.config/sway/custom-cfg
