66d86880f054565596a5ae6362e572f5d8cb910f
schplorg <git@schplorg.de>
2026-05-18 19:14:22 +0200

    see last

--- rm-dummy-eth.sh
+++ rm-dummy-eth.sh
@@ -5,11 +5,18 @@ set -euo pipefail
 
 source .env
 
+DUMMY_OFFLINE="${DUMMY_OFFLINE:-false}"
+
 SUBNET="192.168.42.0/24"
 
-iptables -t nat -D POSTROUTING -s "$SUBNET" -o "$LAN_ADAPTER" -j MASQUERADE || true
-iptables -D FORWARD -i "$LAN_ADAPTER" -o "$HOST_IF" -m state --state RELATED,ESTABLISHED -j ACCEPT || true
-iptables -D FORWARD -i "$HOST_IF" -o "$LAN_ADAPTER" -j ACCEPT || true
+if [ "$DUMMY_OFFLINE" != "true" ]; then
+    iptables -t nat -D POSTROUTING -s "$SUBNET" -o "$LAN_ADAPTER" -j MASQUERADE || true
+    iptables -D FORWARD -i "$LAN_ADAPTER" -o "$HOST_IF" -m state --state RELATED,ESTABLISHED -j ACCEPT || true
+    iptables -D FORWARD -i "$HOST_IF" -o "$LAN_ADAPTER" -j ACCEPT || true
+else
+    iptables -D FORWARD -i "$HOST_IF" -o "$LAN_ADAPTER" -j DROP || true
+    iptables -D FORWARD -i "$LAN_ADAPTER" -o "$HOST_IF" -j DROP || true
+fi
 
 ip link delete "$HOST_IF" || true
 ip link delete "$DUMMY_ADAPTER" || true
