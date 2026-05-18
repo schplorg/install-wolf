60ba5b377fc9a1434de14e8fb7141c92fca3cfed
schplorg <git@schplorg.de>
2026-05-18 19:11:29 +0200

    add offline flag

--- create-dummy-eth.sh
+++ create-dummy-eth.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 
 source .env
 
+DUMMY_OFFLINE="${DUMMY_OFFLINE:-false}"
+
 SUBNET="192.168.42.0/24"
 GATEWAY="192.168.42.1"
 
@@ -17,6 +19,12 @@ ip link set "$HOST_IF" up
 
 sysctl -w net.ipv4.ip_forward=1
 
-iptables -t nat -A POSTROUTING -s "$SUBNET" -o "$LAN_ADAPTER" -j MASQUERADE
-iptables -A FORWARD -i "$LAN_ADAPTER" -o "$HOST_IF" -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i "$HOST_IF" -o "$LAN_ADAPTER" -j ACCEPT
+if [ "$DUMMY_OFFLINE" != "true" ]; then
+    iptables -t nat -A POSTROUTING -s "$SUBNET" -o "$LAN_ADAPTER" -j MASQUERADE
+    iptables -A FORWARD -i "$LAN_ADAPTER" -o "$HOST_IF" -m state --state RELATED,ESTABLISHED -j ACCEPT
+    iptables -A FORWARD -i "$HOST_IF" -o "$LAN_ADAPTER" -j ACCEPT
+else
+    iptables -A FORWARD -i "$HOST_IF" -o "$LAN_ADAPTER" -j DROP
+    iptables -A FORWARD -i "$LAN_ADAPTER" -o "$HOST_IF" -j DROP
+    echo "Offline mode: internet access blocked for $SUBNET"
+fi
